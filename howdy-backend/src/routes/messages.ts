1a418dbc94b9429df6a04e9a45d981adcf6354ce
Hannah Nguyen <hnguyen160801@gmail.com>
2023-09-11 02:32:53 -0400

    add message routes

--- howdy-backend/src/routes/messages.ts
+++ howdy-backend/src/routes/messages.ts
@@ -11,7 +11,37 @@ const router = express.Router();
 
 //get messages from chat id
 router.get("/:id", ensureCorrectUser, async function (req, res, next) {
-  const chat
+  const chat = await prisma.chat.findUnique({
+    where: {
+      id: Number(req.params.id)
+    },
+    include: {
+      messages: {
+        orderBy: {
+          timestamp: 'desc'
+        }
+      }
+    }
+  });
+
+  const messages = chat.messages;
+
+  return res.json({ messages: messages });
+});
+
+//create message in specified chat
+router.post("/:id", ensureCorrectUser, async function (req, res, next) {
+  const userId = res.locals.user.id;
+
+  const message = await prisma.message.create({
+    data: {
+      authorId: userId,
+      chatId: Number(req.params.id),
+      contents: req.body.messageContent
+    }
+  })
+
+  return res.json({message: message})
 })
 
 export { router as messageRoutes };
